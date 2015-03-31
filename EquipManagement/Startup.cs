c3a0114be2cf9ca423ee204a5b36e400de96d811
wudizhanche1000 <wudizhanche1000@gmail.com>
2015-03-31 10:32:02 +0800

    添加初始化

--- EquipManagement/Startup.cs
+++ EquipManagement/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using EquipManagement.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(EquipManagement.Startup))]
@@ -8,6 +11,21 @@ namespace EquipManagement
     {
         public void Configuration(IAppBuilder app)
         {
+            var context = new ApplicationDbContext();
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            if (!RoleManager.RoleExists("Admin"))
+            {
+                RoleManager.Create(new IdentityRole("Admin"));
+                RoleManager.Create(new IdentityRole("Teacher"));
+
+            }
+            if (UserManager.FindByName("admin@admin.com") == null)
+            {
+                var user = new ApplicationUser { Name = "管理员", UserName = "admin@admin.com", Email = "admin@admin.com" };
+                UserManager.Create(user, "administrator");
+                UserManager.AddToRole(UserManager.FindByName(user.UserName).Id, "Admin");
+            }
             ConfigureAuth(app);
         }
     }
